b2c3ffa78cc8262eddd2e074b44574d1a83edd4e
jeffliang0318 <liangnash0318@gmail.com>
2020-12-19 23:39:51 -0800

    Add methods syntax

--- README.md
+++ README.md
@@ -8,16 +8,45 @@ To use EventCounter library
 import { EventCounter } from './event-counter';
 ```
 
+### EventCounter
+#### Syntax
+```
+const eventCounter = new EventCounter(eventName)
+```
+#### Parameter Value
+| Parameter |Description |
+| ------------- | ------------- |
+| eventName: String  | Optional. The name of the event counter. Defaults to `event` |
+
+### getCount
+#### Syntax
+```
+eventCounter.getCount(timeWindow)
+```
+#### Parameter Value
+| Parameter  | Description |
+| ------------- | ------------- |
+| timeWindow: Number | Optional. The amount of time until current time in seconds. Defaults to `300`|
+
+### incrementCount
+#### Syntax
+```
+eventCounter.incrementCount()
+```
+#### Parameter Value
+None
+
 ## Usage 
 ```javascript
-const eventCounter = EventCounter();   //
+const eventCounter = new EventCounter();    //
 eventCounter.getCount()                     // return 0
-eventCounter.incrementCount();         // return "event just happened"
+eventCounter.incrementCount();              // return "event just happened"
 eventCounter.getCount()                     // return 1
 
 // after 1 minute (60 seconds)
 // return events happened in 5 minute if not specify the time window
 eventCounter.getCount()                     // return 1                     
+
 // get the number of the events happened in 30 seconds
 eventCounter.getCount(30)                   // return 0
 
--- event-counter.js
+++ event-counter.js
@@ -2,71 +2,79 @@
 const TIME_WINDOW_UPPER_BOUND = 300;
 
 export class EventCounter {
-    constructor(name = 'event') {
-        if (typeof name !== "string") {
-            throw new TypeError("Invalid name, should be string.")
-        }
-        this.name = name;
-        this.eventTimestamps = [];
-        this.timeUpperbond = TIME_WINDOW_UPPER_BOUND;
+  constructor(name = "event") {
+    if (typeof name !== "string") {
+      throw new TypeError("Invalid name, should be string.");
     }
+    this.name = name;
+    this.eventTimestamps = [];
+    this.timeUpperbond = TIME_WINDOW_UPPER_BOUND;
+  }
 
-    /**
-     * Add an event timestamp and notify client
-     * @returns {String} notify client an event just happened.
-    */
-    incrementCount () {
-        // We can save some memory by clean the outdated event(s) in eventTimestamps array
-        // everytime we increment the count
-        this.cleanExpiredTimestamps();
-        this.eventTimestamps.push(new Date().getTime());
-        return `${this.name} just happened`;
-    }
+  /**
+   * Add an event timestamp and notify client
+   * @returns {String} notify client an event just happened.
+   */
+  incrementCount() {
+    // We can save some memory by clean the outdated event(s) in eventTimestamps array
+    // everytime we increment the count
+    this.cleanExpiredTimestamps();
+    this.eventTimestamps.push(new Date().getTime());
+    return `${this.name} just happened`;
+  }
 
-    /**
-     * Return number of events happened in timewindow
-     * @param {Number} timeWindow - client specified amount of time default to TIME_WINDOW_UPPER_BOUND
-     * @returns {Number} the length of the filterd timestamp array 
-    */
-    getCount (timeWindow = this.timeUpperbond) {
-        this.isTimeWindowValid(timeWindow)
-        const strictTimeWindow = timeWindow > this.timeUpperbond ? this.timeUpperbond : timeWindow;
-        const count = this.getRecentEvents(strictTimeWindow).length;
-        console.log(`${this.name} happened ${count} times in ${strictTimeWindow} seconds`);
-        return count
-    }
-    
-    //private methods
-    // --------------
-    /**
-     * Return number of events happened in timewindow but NOT mutate the timestamp array
-     * @param {Number} timeWindow - client specified amount of time, default to TIME_WINDOW_UPPER_BOUND
-     * @returns {Number} the length of the filterd timestamp array
-    */
-    getRecentEvents (timeWindow = this.timeUpperbond) {
-        const currTime = new Date().getTime();
-        // return a new array but not mutate the eventTimestamps
-        return this.eventTimestamps.filter( time => currTime - time < timeWindow * 1000);
-    }
+  /**
+   * Return number of events happened in timewindow
+   * @param {Number} timeWindow - client specified amount of time default to TIME_WINDOW_UPPER_BOUND
+   * @returns {Number} the length of the filterd timestamp array
+   */
+  getCount(timeWindow = this.timeUpperbond) {
+    this.isTimeWindowValid(timeWindow);
+    const strictTimeWindow =
+      timeWindow > this.timeUpperbond ? this.timeUpperbond : timeWindow;
+    const count = this.getRecentEvents(strictTimeWindow).length;
+    console.log(
+      `${this.name} happened ${count} times in ${strictTimeWindow} seconds`
+    );
+    return count;
+  }
 
-    /**
-     * Update eventTimestamps by romeve the event timestamp(s) happened 300 seconds ago and 
-     */
-    cleanExpiredTimestamps () {
-        this.eventTimestamps = this.getRecentEvents();
-    }
+  //private methods
+  // --------------
+  /**
+   * Return number of events happened in timewindow but NOT mutate the timestamp array
+   * @param {Number} timeWindow - client specified amount of time, default to TIME_WINDOW_UPPER_BOUND
+   * @returns {Number} the length of the filterd timestamp array
+   */
+  getRecentEvents(timeWindow = this.timeUpperbond) {
+    const currTime = new Date().getTime();
+    // return a new array but not mutate the eventTimestamps
+    return this.eventTimestamps.filter(
+      (time) => currTime - time < timeWindow * 1000
+    );
+  }
 
-    /**
-     * Throw an error if timeWindow is not a number greater than 0
-     */
-    isTimeWindowValid (timeWindow) {
-        if (typeof timeWindow !== "number" || isNaN(timeWindow)) {
-            const wrongType = typeof timeWindow === "number" ? "NaN" : typeof timeWindow
+  /**
+   * Update eventTimestamps by romeve the event timestamp(s) happened 300 seconds ago and
+   */
+  cleanExpiredTimestamps() {
+    this.eventTimestamps = this.getRecentEvents();
+  }
 
-            throw new TypeError(`getCount only takes number type but ${wrongType}`);
-        };
-        if (timeWindow <= 0) {
-            throw new RangeError(`getCount only takes number greater than 0 but ${timeWindow}`);
-        }
+  /**
+   * Throw an error if timeWindow is not a number greater than 0
+   */
+  isTimeWindowValid(timeWindow) {
+    if (typeof timeWindow !== "number" || isNaN(timeWindow)) {
+      const wrongType =
+        typeof timeWindow === "number" ? "NaN" : typeof timeWindow;
+
+      throw new TypeError(`getCount only takes number type but ${wrongType}`);
     }
+    if (timeWindow <= 0) {
+      throw new RangeError(
+        `getCount only takes number greater than 0 but ${timeWindow}`
+      );
+    }
+  }
 };
